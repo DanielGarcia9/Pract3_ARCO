1314473bb3524da55e8d2eceabdc5ae36e86afa0
DanielGarcia9 <91562312+DanielGarcia9@users.noreply.github.com>
2023-04-06 12:12:05 +0200

    Filtro azul y crono

--- Pract3/mainwindow.cpp
+++ Pract3/mainwindow.cpp
@@ -3,15 +3,17 @@
 #include <opencv2/opencv.hpp>
 #include <QFileDialog>
 #include <QString>
+#include <chrono>
 
 using namespace cv;
+using namespace std::chrono;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    
+
 }
 
 MainWindow::~MainWindow()
@@ -19,11 +21,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushBotonPrueba_clicked()
-{
-    QString filePath = QFileDialog::getOpenFileName(this, "Selecciona un archivo", "/home/", tr("JPEG (*.jpg);;PNG (*.png);;Todos los archivos (*)"));
-    Mat image = imread(filePath.toStdString(), IMREAD_COLOR);
 
+void MainWindow::on_pushButton_clicked()
+{
+    QString filePath = QFileDialog::getOpenFileName(this, "Selecciona un archivo", "/home/", tr("JPEG (*.jpg);;PNG (*.png)"));
+    Mat image = imread(filePath.toStdString(), IMREAD_COLOR);
+    auto start = high_resolution_clock::now();
     int filas = image.rows;
     int columnas = image.cols;
 
@@ -33,16 +36,19 @@ void MainWindow::on_pushBotonPrueba_clicked()
             int r = pixelValue[2];
             int g = pixelValue[1];
             int b = pixelValue[0];
+            pixelValue[2] = r*0.5;
+            pixelValue[1] = g*0.5;
+            pixelValue[0] = b*1.5;
 
-            pixelValue[2] = cv::saturate_cast<uchar>(0.393*r + 0.769*g + 0.189*b);  //Es sepia hay que cambiarlo
-            pixelValue[1] = cv::saturate_cast<uchar>(0.349*r + 0.686*g + 0.168*b);
-            pixelValue[0] = cv::saturate_cast<uchar>(0.272*r + 0.534*g + 0.131*b);
       }
     }
-
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
     QString outputFilePath = QFileDialog::getSaveFileName(nullptr, "Guardar imagen", "/home/");
 
     std::string outputDirStr = outputFilePath.toStdString();
     outputDirStr = outputDirStr + ".jpg";
+    std::cout << "Tiempo de ejecución: " << duration.count() << " microsegundos" << std::endl;
     imwrite(outputDirStr, image);
 }
+
