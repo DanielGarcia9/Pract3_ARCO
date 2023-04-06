ccea6f9a4565074251a542a588f71c748bb9d0e3
DanielGarcia9 <91562312+DanielGarcia9@users.noreply.github.com>
2023-04-06 19:29:01 +0200

    Update ordenacion.cpp

--- Pract3/ordenacion.cpp
+++ Pract3/ordenacion.cpp
@@ -26,32 +26,38 @@ Ordenacion::~Ordenacion()
 void Ordenacion::on_pushButton_2_clicked()
 {
     QString filePath = QFileDialog::getOpenFileName(this, "Selecciona un archivo", "/home/", tr("TXT (*.txt)"));
+    QString exitPath = QFileDialog::getOpenFileName(this, "Selecciona un archivo de guardado", "/home/", tr("TXT (*.txt)"));
+
+    auto start = high_resolution_clock::now();
+
     string fileP = filePath.toStdString();
+    string exitP = exitPath.toStdString();
     ifstream file(fileP);
 
-        if (!file)
-        {
-            cerr << "No se pudo abrir el archivo" << endl;
-        }
+    if(!file){
+        cerr << "No se pudo abrir el archivo" << endl;
+    }
 
-        vector<string> words;
-            string word;
-            while (file >> word)
-            {
-                words.push_back(word);
-            }
+    vector<string> words;
+    string word;
+
+    while (file >> word){
+        words.push_back(word);
+    }
 
     file.close();
     sort(words.begin(), words.end());
 
-    //Cambiar path q sino hay lio xd
-    ofstream file2(fileP);
+    ofstream file2(exitP);
 
-    for (const string& word : words)
-        {
-            file2 << word << endl;
-        }
+    for(const string& word : words){
+        file2 << word << endl;
+    }
 
-        file2.close();
+    file2.close();
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Tiempo de ejecución: " << duration.count() << " microsegundos" << endl;
 
 }
